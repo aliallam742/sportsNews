00c52d0cf20624d60bcc45deb9fa8b5d1ebb7ae3
aliallam742 <aliallam7472@gmail.com>
2024-01-11 14:53:10 +0200

    new commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+"use strict"
 var  btnMenu = document.getElementById("btn-menu"); 
 var linksActive = document.getElementById("links-active");
 
@@ -54,7 +55,7 @@ function displayData(){
     })
     
 }
-contries(contry="eg")
+contries("eg")
 
 // var myHttp = new XMLHttpRequest();
 // var data =[];
